d2e9ea9922bb8eb87b584755271d5644c037455d
mikhail.malasai <mikhail.malasai@leverx.com>
2022-06-08 12:36:01 +0300

    Refine KDoc

--- resource-context/src/main/kotlin/com/mmolosay/resource/state/ResourceState.kt
+++ resource-context/src/main/kotlin/com/mmolosay/resource/state/ResourceState.kt
@@ -19,23 +19,7 @@ import com.mmolosay.resource.context.ResourceContext
  */
 
 /**
- * // TODO: update
  * Represents state of dynamically obtained data.
- * It's either [Empty], [Loading], [Success] or [Failure] instance.
- *
- * Example of usage:
- * ```
- * val observable = SomeObservable<Resource<YourData>>()
- * observable.value = Resource.com.mmolosay.resource.state.Empty
- * getData { data ->
- *     observable.value = Resource.com.mmolosay.resource.state.Success(data)
- * }
- * ...
- * ...
- * observable.observe { resource ->
- *     resource.fold(...)
- * }
- * ```
  *
  * @param V the type of data. Although [V] can be both `nullable` and `non-nullable` type,
  * it is __strongly recommended__ to use `non-nullable` ones, since success state with `null` as
